e8d6bd5a9f01135b8ac1384f71e21b57c4bc2989
Damodar Hasti <damodarhasti@gmail.com>
2016-10-16 21:19:59 -0400

    test.java

---
+++ sampleproject/Test.java
@@ -0,0 +1,22 @@
+package sampleproject;
+
+public class Test {
+	
+	//Variable
+	public String name = "Kiran";
+	
+	public void printName(){
+		System.out.println("My Name is: "+ name);
+	}
+	
+	public int additionInterger(int a, int b) {
+		int output;
+		output = a + b;
+
+		System.out.println("Addition is: " + output);
+		return output;
+
+	}
+
+
+}
